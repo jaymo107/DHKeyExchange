ad8c9316685829537ab69176327f9ff6289b1211
James Davies <jaymo107@gmail.com>
2017-02-06 20:21:07 +0000

    Fixed padding error, and refactored

--- com/defence/Main.java
+++ com/defence/Main.java
@@ -1,43 +1,30 @@
 package com.defence;
 
-import com.sun.deploy.util.StringUtils;
-
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
 import java.net.*;
 import java.io.*;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Random;
-
-import static java.awt.SystemColor.text;
 
 public class Main {
 
-    private Random random;
-    private String iv;
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws IOException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
-
-        new Main().initialise(args);
+        // Pass the arguments to the initialise
+        new Main().initialise(args[0], args[1], args[2]);
 
     }
 
-    private void initialise(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, BadPaddingException, InvalidKeyException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+    private void initialise(String input1, String input2, String input3) throws Exception {
 
         // Grab the values from the parameters
-        BigInteger p = BigInteger.valueOf(Long.parseLong(args[0]));
-        BigInteger g = BigInteger.valueOf(Long.parseLong(args[1]));
-        BigInteger a = BigInteger.valueOf(Long.parseLong(args[2]));
+        BigInteger p = new BigInteger(input1);
+        BigInteger g = new BigInteger(input2);
+        BigInteger a = new BigInteger(input3);
 
         // Specify the IP address of the server
         InetAddress addr = InetAddress.getByName("127.0.0.1");
@@ -50,15 +37,22 @@ public class Main {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
+            // Create the random 4 digig nonce
+            String nonce = createNonce();
+
+            // Create the diffe helman shared key
             BigInteger key = generateDHSharedkey(g, a, p, out, in);
-            String sessionKey = createSessionKey(key);
 
-            System.out.println(sessionKey);
+            // Create the IV
+            String iv = createIv(key, nonce);
 
-            // Send the session key
-            out.println("##NONCE##" + sessionKey + "####");
+            byte[] sessionKey = createSessionKey(iv);
 
-            String file = requestEncryptedFile(out, in, sessionKey);
+            // Send the nonce
+            out.println("##NONCE##" + nonce + "####");
+            out.flush();
+
+            String file = requestEncryptedFile(out, in, iv, sessionKey);
 
             System.out.println(file);
 
@@ -70,7 +64,16 @@ public class Main {
         }
     }
 
-    private BigInteger generateDHSharedkey(BigInteger g, BigInteger a, BigInteger p, PrintWriter out, BufferedReader in) throws IOException {
+    /**
+     * @param g
+     * @param a
+     * @param p
+     * @param out
+     * @param in
+     * @return
+     * @throws Exception
+     */
+    private BigInteger generateDHSharedkey(BigInteger g, BigInteger a, BigInteger p, PrintWriter out, BufferedReader in) throws Exception {
 
         // Create A to send to the server
         String A = "##DHA##" + (g.pow(a.intValue()).mod(p)).toString() + "####";
@@ -81,89 +84,101 @@ public class Main {
         // Read in the response
         String str = in.readLine();
 
+        // Remove everything except numbers, extract the value from the response
         BigInteger B = BigInteger.valueOf(Long.valueOf(str.replaceAll("[^\\d]", "")));
 
-        System.out.println("String from server (B): " + B.toString());
-
-        BigInteger key = (B.pow(a.intValue()).mod(p));
-
-        System.out.println("Shared key is: " + key);
-
-        return key;
+        return (B.pow(a.intValue()).mod(p));
     }
 
-    private String createSessionKey(BigInteger dhKey) {
-
-        int padAmount = 12;
+    /**
+     * Create the 4 digit nonce to pass to the server.
+     *
+     * @return
+     */
+    private String createNonce() {
 
         // Generate random 4 digit number
         int R = (int) (Math.random() * 9000) + 1000;
 
-        String paddedKey = String.format("%0" + padAmount + "d", dhKey.intValue());
+        return String.valueOf(R);
+    }
 
-        System.out.println("Random number (R): " + R);
+    /**
+     * Create the IV consistint of the diffeHelman and nonce key padded using 12 0's
+     *
+     * @param dhKey
+     * @param R
+     * @return String
+     * @throws Exception
+     */
+    private String createIv(BigInteger dhKey, String R) throws Exception {
+
+        // Pad the key with zeros
+        String paddedKey = String.format("%012d", dhKey.intValue());
 
         // Prepend the random number to the beginning of the key
-        String newKey = String.valueOf(R) + paddedKey;
-        System.out.println("Concatenated key: " + newKey);
+        return R + paddedKey;
 
-        this.iv = newKey;
+    }
 
-        MessageDigest digest = null;
-
+    /**
+     * Has the IV key using SHA-256 and take the first 16 bytes.
+     *
+     * @param concatenatedKey
+     * @return byte[]
+     */
+    private byte[] createSessionKey(String concatenatedKey) {
         try {
-            digest = MessageDigest.getInstance("SHA-256");
-            byte[] hashed = digest.digest(newKey.getBytes("UTF-8"));
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashed = digest.digest(concatenatedKey.getBytes("UTF-8"));
 
-            // Get first 16 bytes
-            byte[] first16Bytes = Arrays.copyOfRange(hashed, 0, 16);
+            // Return first 16 bytes
+            return Arrays.copyOf(hashed, 16);
 
-            StringBuilder hexString = new StringBuilder();
-
-            for (byte first16Byte : first16Bytes) {
-                String hex = Integer.toHexString(0xff & first16Byte);
-                if (hex.length() == 1)
-                    hexString.append('0');
-                hexString.append(hex);
-            }
-
-            System.out.println("Hash: " + hexString.substring(0, 16));
-            return hexString.substring(0, 16);
-
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return "";
+        return new byte[0];
     }
 
-    private String requestEncryptedFile(PrintWriter out, BufferedReader in, String key) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+
+    /**
+     * @param out
+     * @param in
+     * @return
+     * @throws Exception
+     */
+    private String requestEncryptedFile(PrintWriter out, BufferedReader in, String iv, byte[] sessionKey) throws Exception {
         // Request a file
         out.println("##REQFILE####");
 
         String response = in.readLine();
-
-        System.out.println("File response: " + response);
-
         String fileContents = response.substring(11, response.indexOf("####"));
-        System.out.println("File contents: " + fileContents);
 
-        System.out.println("IV should be 16 characters long, actual is: " + this.iv.length());
+        // Decrypt the data
+        byte[] data = decrypt(fileContents, iv, sessionKey);
 
-        IvParameterSpec iv = new IvParameterSpec(this.iv.getBytes("UTF-8"));
-        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+        // Decrypt the data
+        return new String(data);
+    }
+
+    /**
+     * @param encrypted The BASE64 decoded message to decrypt
+     * @return byte[]   The decrypted bytes
+     * @throws Exception
+     */
+    private byte[] decrypt(String encrypted, String iv, byte[] sessionKey) throws Exception {
+
+        IvParameterSpec ivSpec = new IvParameterSpec(iv.getBytes());
+        SecretKeySpec skeySpec = new SecretKeySpec(sessionKey, "AES");
 
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
-        byte[] original = cipher.doFinal(Base64.getDecoder().decode(fileContents));
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivSpec);
+        byte[] decoded = Base64.getDecoder().decode(encrypted);
 
-        //return new String(original);
-
-        //byte[] original = cipher.doFinal(decoded);
-        return "";
-        //return new String(original);
-
+        return cipher.doFinal(decoded);
     }
 
 }
